5daffac882fece597c8870647a872b581c617e45
Diana Nagy <Diana Nagy@LAPTOP-4OFFJGK1.home>
2017-08-21 12:11:28 +0100

    Nice

--- postCreater.php
+++ postCreater.php
@@ -1,6 +1,6 @@
 <?php
 $url='postTarget.php';
-$body='param=123';
+$body='param=123';   
 $options=array('method'=>'post','content'=>$body,'header' => 'Content-type: application/x-www-form-urlencoded');
 $context = stream_context_create(array('http' => $options));
 // Pass the context to file_get_contents()
